79b1f01795945b4a7aebed0a2b0579867b89d14a
VictoireMukovi <victoirepalukumukovi@gmail.com>
2024-03-04 21:28:58 +0100

    Ajout de get et post

--- api.py
+++ api.py
@@ -23,5 +23,25 @@ task_update_args=reqparse.RequestParser()
 task_update_args.add_argument("task",type=str)
 task_update_args.add_argument("summary",type=str)
 
+ressource_fields={
+    '_id':fields.Integer,
+    'task':fields.String,
+    'summary':fields.String
+}
+
+class ToDo(Resource):
+    @marshal_with(ressource_fields)
+    def get(self,todo_id):
+        task=Todomodel.Objects.get(_id=todo_id)
+        if not task:
+            abort(404,message="could not find task with that id")
+        return task
+    @marshal_with(ressource_fields)
+    def post(self,todo_id):
+        args=task_pos_args.parse_args()
+        todo=Todomodel(_id=todo_id,task=args['task'],summary=args['summary']).save()
+        id_=todo._id
+        return {"id":str(id_)},201
+
 if __name__=='__main':
     app.run(debug=True)
